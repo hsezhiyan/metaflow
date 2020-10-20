7c3ef5026da86d9f7aa11feaf6c66fb665b17905
Hari Sezhiyan <hariharans@zg06955.zillow.local>
2020-10-20 12:49:40 -0700

    path changes, hopefully work.

--- dodo.py
+++ dodo.py
@@ -16,19 +16,13 @@ def task_run_integration_tests():
             "docker run --rm -v $(pwd):/home/zservice "
             + "hsezhiyan/metaflow-integration-testing:1.0 "
             + "bash -c '"
-            # + "export METAFLOW_USER=compile_only_user "
-            # + "&& export METAFLOW_DATASTORE_SYSROOT_S3=s3://aip-example-dev/metaflow"
-            # + "&& mkdir -p $(dirname %(mnt_path)s/%(compiled_pipeline_path)s) "
-            # + "&& python aip_metaflow_example/branch_flow.py --no-pylint --datastore s3 kfp run "
-            # + "  --yaml-only --no-s3-code-package --base-image %(target_image)s --pipeline-name %(name)s "
-            # + "  --pipeline-path %(mnt_path)s/%(compiled_pipeline_path)s'",
             + "export KFP_RUN_URL_PREFIX=https://kubeflow.corp.dev-k8s.zg-aip.net/ && "
             + "export KFP_SDK_NAMESPACE=aip-example && "
             + "export METAFLOW_DATASTORE_SYSROOT_S3=s3://aip-example-dev/metaflow/ && "
             + "export METAFLOW_DEFAULT_DATASTORE=local && "
             + "export METAFLOW_USER=hariharans@zillowgroup.com && "
-            + "pip install -e /home/zservice/metaflow && "
-            + "cd /home/zservice/metaflow/metaflow/plugins/kfp/tests && "
+            + "pip install -e /home/zservice && "
+            + "cd /home/zservice/metaflow/plugins/kfp/tests && "
             + "python -m pytest -s -n 2 run_integration_tests.py'"
         ],
     }
