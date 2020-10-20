3004fe241c1f381e2b5452418f2986fa07abeef8
Hari Sezhiyan <hariharans@zg06955.zillow.local>
2020-10-20 15:53:25 -0700

    Check to see if s3 upload works.

--- dodo.py
+++ dodo.py
@@ -27,8 +27,6 @@ def task_run_integration_tests():
             + "metaflow-integration-testing:1.0 "
             + "bash -c '"
             + "aws sts get-caller-identity && "
-            + "aws sts assume-role --role-arn arn:aws:sts::236352297023:role/kubeflow-generic-role --role-session-name s3-access && "
-            + "aws sts get-caller-identity && "
             + "export KFP_RUN_URL_PREFIX=https://kubeflow.corp.dev-k8s.zg-aip.net/ && "
             + "export KFP_SDK_NAMESPACE=metaflow-integration-testing && "
             + "export METAFLOW_DATASTORE_SYSROOT_S3=s3://aip-example-dev/metaflow/ && "
