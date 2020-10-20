8d6cc05f72e4b5156047826612ec9e2d8df3c41b
Hari Sezhiyan <hariharans@zg06955.zillow.local>
2020-10-20 13:12:27 -0700

    Updating dodo

--- dodo.py
+++ dodo.py
@@ -10,22 +10,20 @@ def _metaflow_kube_mounts():
 # TODO AIP-1766 Move compile_pipeline task to aip-doit-modules
 def task_run_integration_tests():
     """Compiles pipeline."""
-    _default_pipeline_name = "aip-metaflow-example-train"
     return {
         "actions": [
             "docker run --rm -v $(pwd):/home/zservice "
             + "hsezhiyan/metaflow-integration-testing:1.0 "
             + "bash -c '"
             + "ls && "
-            + "ls /home/zservice'"
-            # + "export KFP_RUN_URL_PREFIX=https://kubeflow.corp.dev-k8s.zg-aip.net/ && "
-            # + "export KFP_SDK_NAMESPACE=aip-example && "
-            # + "export METAFLOW_DATASTORE_SYSROOT_S3=s3://aip-example-dev/metaflow/ && "
-            # + "export METAFLOW_DEFAULT_DATASTORE=local && "
-            # + "export METAFLOW_USER=hariharans@zillowgroup.com && "
-            # + "pip install -e /home/zservice && "
-            # + "cd /home/zservice/metaflow/plugins/kfp/tests && "
-            # + "python -m pytest -s -n 2 run_integration_tests.py'"
+            + "export KFP_RUN_URL_PREFIX=https://kubeflow.corp.dev-k8s.zg-aip.net/ && "
+            + "export KFP_SDK_NAMESPACE=aip-example && "
+            + "export METAFLOW_DATASTORE_SYSROOT_S3=s3://aip-example-dev/metaflow/ && "
+            + "export METAFLOW_DEFAULT_DATASTORE=local && "
+            + "export METAFLOW_USER=hariharans@zillowgroup.com && "
+            + "pip install -e /home/zservice && "
+            + "cd /home/zservice/metaflow/plugins/kfp/tests && "
+            + "python -m pytest -s -n 2 run_integration_tests.py'"
         ],
     }
 
