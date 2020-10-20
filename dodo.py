b2c39a78367ccf17f354f5c3e1a39c6f9e074e1c
Hari Sezhiyan <hariharans@zg06955.zillow.local>
2020-10-20 14:17:11 -0700

    Docker in 2 stage: build, run

--- dodo.py
+++ dodo.py
@@ -7,22 +7,26 @@ def _metaflow_kube_mounts():
        + ("-v /root/.kube:/home/zservice/.kube " if (Path.home() / Path(".kube")).exists() else "")
        + ("-v /root/.aws:/home/zservice/.aws " if (Path.home() / Path(".aws")).exists() else ""))
 
+def task_build_docker_image():
+    return {
+        "actions": [
+            "docker build --tag metaflow-integration-testing:1.0 ."
+        ],
+    }
+
 # TODO AIP-1766 Move compile_pipeline task to aip-doit-modules
 def task_run_integration_tests():
     """Compiles pipeline."""
     return {
         "actions": [
-            "docker run --rm -v /builds/analytics/artificial-intelligence/ai-platform/aip-workflow/aip-metaflow-cicd-integration:/home/zservice "
-            + "hsezhiyan/metaflow-integration-testing:1.0 "
+            "docker run --rm metaflow-integration-testing:1.0 "
             + "bash -c '"
-            + "ls && "
             + "export KFP_RUN_URL_PREFIX=https://kubeflow.corp.dev-k8s.zg-aip.net/ && "
             + "export KFP_SDK_NAMESPACE=aip-example && "
             + "export METAFLOW_DATASTORE_SYSROOT_S3=s3://aip-example-dev/metaflow/ && "
             + "export METAFLOW_DEFAULT_DATASTORE=local && "
             + "export METAFLOW_USER=hariharans@zillowgroup.com && "
-            + "pip install -e /home/zservice && "
-            + "cd /home/zservice/metaflow/plugins/kfp/tests && "
+            + "cd /metaflow/metaflow/plugins/kfp/tests && "
             + "python -m pytest -s -n 2 run_integration_tests.py'"
         ],
     }
