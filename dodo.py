a18c5f9f1585cba150410294a654e6a109bcbc34
Hari Sezhiyan <hariharans@zg06955.zillow.local>
2020-10-21 12:39:01 -0700

    Testing to custom namespace.

--- dodo.py
+++ dodo.py
@@ -18,10 +18,10 @@ def task_run_integration_tests():
             + "metaflow-integration-testing:1.0 "
             + "bash -c '"
             + "export KFP_RUN_URL_PREFIX=https://kubeflow.corp.dev-k8s.zg-aip.net/ && "
-            + "export KFP_SDK_NAMESPACE=aip-example && "
+            + "export KFP_SDK_NAMESPACE=metaflow-integration-testing && "
             + "export METAFLOW_DATASTORE_SYSROOT_S3=s3://aip-example-dev/metaflow/ && "
             + "export METAFLOW_DEFAULT_DATASTORE=local && "
-            + "export METAFLOW_USER=talebz@zillowgroup.com && "
+            + "export METAFLOW_USER=hariharans@zillowgroup.com && "
             + "cd /metaflow/metaflow/plugins/kfp/tests && "
             + "python -m pytest -s -n 2 run_integration_tests.py'"
         ],
