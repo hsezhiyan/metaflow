1c84af8487e48c7ec1b0f8ec77b33cb8ab2d524e
Hari Sezhiyan <hariharans@zg06955.zillow.local>
2020-10-21 14:31:24 -0700

    Moving Dockerfile to the correct place.

--- dodo.py
+++ dodo.py
@@ -1,13 +1,13 @@
 def task_build_docker_image():
     return {
         "actions": [
-            "docker build -f integration_testing_image/Dockerfile -t metaflow-integration-testing:1.0 ."
+            "docker build -f metaflow/plugins/kfp/tests/Dockerfile -t metaflow-integration-testing:1.0 ."
         ],
     }
 
 # TODO AIP-1766 Move compile_pipeline task to aip-doit-modules
 def task_run_integration_tests():
-    """Compiles pipeline."""
+    """Run the full integration suite inside a Docker container."""
     return {
         "actions": [
             "docker run --rm "
